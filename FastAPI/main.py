3d793309fedec7403abe52b76e30d0d39e3901f0
Архипов Владимир <arhipvp@gmail.com>
2023-07-01 22:40:15 +0300

    Переделал на асинхрон

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,6 +1,7 @@
+import asyncio
 import os
 
-import requests
+from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
 from fastapi import FastAPI
@@ -8,16 +9,21 @@ from fastapi import FastAPI
 load_dotenv()
 
 app = FastAPI()
+
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 MESSAGE = 'Кто-то трогает API!'
+bot = Bot(TELEGRAM_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def send_message():
+    await bot.send_message(CHAT_ID, MESSAGE)
+
+
 
 @app.get('/')
-def hello():
-    
-    url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
-    data = {'chat_id': CHAT_ID, 'text': MESSAGE}
-    response = requests.post(url, data=data)
-
-    
-    return response.json()
+async def hello():
+    asyncio.get_running_loop().create_task(send_message())
+    return MESSAGE
